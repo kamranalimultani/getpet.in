2a42eea8ce662ca12b81100a7c1e41a6b5c9b378
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-05-19 01:57:08 +0530

    chnaegs

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Welcome');
+    return view('welcome');
 });
 Route::get('/test', function () {
     return view('MainSite.Home.index');
